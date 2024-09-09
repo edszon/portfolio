64d91af353e7c8a08ef4f7cf8b7db314d831c62d
edson <edszon.cavalcanti@gmail.com>
2024-09-09 02:24:46 -0300

    landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig(
 {
-    site: 'https://edszon.github.io',
-    base: 'portfolio',
-
+  site: 'https://edszon.github.io',
+  base: 'portfolio',
+  integrations: [tailwind()],
 });
