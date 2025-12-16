3c4ce2daa2049fa56ab9b431887e28a6abc1cf4f
Edson Cavalcanti <edson.cavalcanti.117@ufrn.edu.br>
2025-12-16 15:01:55 -0300

    fix: restore CNAME for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig(
-{
-  site: 'https://edszon.github.io',
-  integrations: [tailwind(), icon()],
-});
+  {
+    site: 'https://edszon.com',
+    integrations: [tailwind(), icon()],
+  });
